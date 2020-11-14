cf9e37d12d0f12670ff72dd5867c2995041b1264
Amel MallemLouda <amelmalleml2016@gmail.com>
2020-11-13 21:24:40 -0500

    first commit

---
+++ Challenge1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge1
+{
+    [TestClass]
+    public class Challenge1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string mylastname = "Mallem Louda";
+            string myfirstname = "Amel";
+            int myage = 27;
+            Console.WriteLine(mylastname+"\n "+myfirstname+" \n"+myage);
+        }
+        [TestMethod]
+        public void MyTestMethod2()
+
+        {
+            string[] favoritemovies = { "Holidate", "jurassic park", "jumaji", "El camino" } ;
+            foreach(string i in favoritemovies)
+            Console.WriteLine(i);
+        }
+        [TestMethod]
+        public void MyTestMethod3()
+        {
+            List<DateTime> date= new List<DateTime>();
+            DateTime adate = new DateTime(2005, 10, 12);
+            DateTime justDate = new DateTime(2002, 10, 18);
+            DateTime today = DateTime.Now;
+            date.Add(today);
+            date.Add(adate);
+            date.Add(justDate);
+            foreach(DateTime dateTime in date)
+                Console.WriteLine(dateTime);
+        }
+       
+        [TestMethod]
+        public void MyTestMethod4()
+        {
+            int myAge = 27;
+            int numTwo = 7;
+            int sum = myAge + numTwo;
+            Console.WriteLine(sum);
+        }
+        [TestMethod]
+        public void MyTestMethod5()
+        {
+            int hoursOfSleep= 8;
+
+            if (hoursOfSleep >= 10)
+
+            {
+                Console.WriteLine("Wow that's a lot of sleep");
+            }
+
+            else if (hoursOfSleep > 8 && hoursOfSleep < 10)
+            {
+                Console.WriteLine("You should be pretty rested");
+            }
+            else if (hoursOfSleep > 4 && hoursOfSleep < 8)
+            {
+                Console.WriteLine("Bummer");
+            }
+            else
+            {
+                Console.WriteLine("Oh man,get some seep"  );
+            }
+          
+        }
+        [TestMethod]
+        public void MyTestMethod6()
+        {
+            string daymood = "Great";
+
+            switch(daymood)
+            {
+                case "Great":
+                    Console.WriteLine("GREAT");
+                    break;
+                case "GOOD":
+                    Console.WriteLine("Good");
+                    break;
+                case "Okay":
+                    Console.WriteLine("Okay");
+                    break;
+                case "Bad":
+                    Console.WriteLine("Bad");
+                    break;
+
+            }
+
+                
+
+
+                
+        }
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18345212-a82c-45fc-8b50-9c242d806bb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
